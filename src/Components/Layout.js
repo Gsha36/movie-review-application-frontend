f64b2ab2044d5bcaecefcf43a3f3869960c26bdb
Gouri Sharma <gouri.sha36@gmail.com>
2024-06-27 21:52:50 +0530

    Initial Commit
    React for frontend

---
+++ src/Components/Layout.js
@@ -0,0 +1,14 @@
+// ./Components/Layout.js
+
+import { Outlet } from 'react-router-dom';
+import React from 'react';
+
+const Layout = () => {
+    return (
+        <main>
+            <Outlet />
+        </main>
+    );
+};
+
+export default Layout;
